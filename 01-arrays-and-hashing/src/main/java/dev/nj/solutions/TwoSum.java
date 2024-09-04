e7d405f9bd6d7471f8b794a7fb12faaeafb9c7f3
eucarizan <eucarizan@gmail.com>
2024-09-05 00:23:48 +0800

    01 two sum, hashmap

--- 01-arrays-and-hashing/src/main/java/dev/nj/solutions/TwoSum.java
+++ 01-arrays-and-hashing/src/main/java/dev/nj/solutions/TwoSum.java
@@ -1,7 +1,27 @@
 package dev.nj.solutions;
 
+import java.util.HashMap;
+
 public class TwoSum {
+
     public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
+            }
+            
+            map.put(num, i);
+        }
+
+        return new int[] {};
+    }
+
+    public static int[] twoSumBF(int[] nums, int target) {
         int[] result = {0, 1};
 
         for (int i = 0; i < nums.length - 1; i++) {
--- 01-arrays-and-hashing/src/test/java/dev/nj/solutions/TwoSumTests.java
+++ 01-arrays-and-hashing/src/test/java/dev/nj/solutions/TwoSumTests.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TwoSumTests {
 
     @Test
-    public void testMethod() {
+    public void testBasic() {
         int[] nums = {1, 2, 3};
         int target = 4;
 
@@ -27,12 +27,52 @@ public class TwoSumTests {
     }
 
     @Test
-    public void testSample2() {
+    public void testRepeatedElements() {
         int[] nums = {3, 3};
         int target = 6;
 
         int[] expected = {0, 1};
 
+        assertThat(TwoSum.twoSum(nums, target)).isEqualTo(expected);
+    }
+
+    @Test
+    public void testNegative() {
+        int[] nums = {-3, 4, 3, 90};
+        int target = 0;
+
+        int[] expected = {0, 2};
+        
+        assertThat(TwoSum.twoSum(nums, target)).isEqualTo(expected);
+    }
+
+    @Test
+    public void testLargeNumbers() {
+        int[] nums = {1000000, 500, 1000, 999500};
+        int target = 1000000;
+
+        int[] expected = {1, 3};
+        
+        assertThat(TwoSum.twoSum(nums, target)).isEqualTo(expected);
+    }
+
+    @Test
+    public void testSinglePairBothEnds() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 20};
+        int target = 21;
+
+        int[] expected = {0, 9};
+        
+        assertThat(TwoSum.twoSum(nums, target)).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAllNegative() {
+        int[] nums = {-7, -2, -9, -5, -8};
+        int target = -7;
+
+        int[] expected = {1, 3};
+        
         assertThat(TwoSum.twoSum(nums, target)).isEqualTo(expected);
     }
 }
